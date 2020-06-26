4b0c668314ad02791123d842619ad55b2da832ed
satyamkr1402 <59774782+satyamkr1402@users.noreply.github.com>
2020-06-26 16:19:54 +0530

    Create mail.py

---
+++ mail.py
@@ -0,0 +1,16 @@
+#!/bin/bash/python3
+import smtplib
+
+s=smtplib.SMTP('smtp.gmail.com',587)
+
+s.starttls()
+
+s.login("satyamssingh1402@gmail.com","Singh@1402")
+
+subject1 = 'Important'
+
+message1 = "Your code has been failed, please debug it asap."
+
+s.sendmail("satyamsingh1402@gmail.com","satyamkr.gaya@gmail.com",message1)
+
+s.quit()
